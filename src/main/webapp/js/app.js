63fff38060d86d2c32d20d1da90f089f104e81ea
Dimka0777 <sdimka0777@gmail.com>
2017-02-26 00:38:12 +0200

    Add sign in google+ to work

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -86,7 +86,7 @@ app.constant('HTTP_ERRORS', {
  */
 app.factory('oauth2Provider', function ($modal) {
     var oauth2Provider = {
-        CLIENT_ID: 'replace with your client id',
+        CLIENT_ID: '433520313746-5a483d1pqm83iiu4igesvteh1vr625ni.apps.googleusercontent.com',
         SCOPES: 'https://www.googleapis.com/auth/userinfo.email profile',
         signedIn: false
     };
